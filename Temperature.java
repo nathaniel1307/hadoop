19db28cc54ecca7d7bfafbe003a1f9b623e7bcb1
Nathaniel Olson <natsolson@gmail.com>
2019-11-06 17:36:58 +0000

    changed reducer

--- Temperature.java
+++ Temperature.java
@@ -39,13 +39,7 @@ public class Temperature {
 		private IntWritable result = new IntWritable();
 		
 		public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
-			// int sum = 0;
-			// for (IntWritable val : values) {
-			// 	sum += val.get();
-			// }
-            // result.set(sum);
-            values.iterator.next();
-			context.write(key, null);
+			context.write(key, values.iterator().next());
 		}
 	}
 
