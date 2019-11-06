579f02cc57ee882687f28583d0c045213a34a0d9
Nathaniel Olson <natsolson@gmail.com>
2019-11-06 16:50:56 +0000

    Changed name

---
+++ Temperature.java
@@ -0,0 +1,55 @@
+import java.io.IOException;
+import java.util.StringTokenizer;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+public class Temperature {
+	public static class WCMapper extends Mapper<Object, Text, Text, IntWritable>{
+		private final static IntWritable one = new IntWritable(1);
+		
+		//private Text word = new Text();
+		
+		public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+            String line = value.toString();
+            String[] words = line.split(",");
+            if((words[0].equals("UK000056225"))|(words[0].equals("UK000003377"))){
+
+            }
+
+
+		}
+	} 
+	public static class WCReducer extends Reducer<Text,IntWritable,Text,IntWritable> {
+	
+		private IntWritable result = new IntWritable();
+		
+		public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+			// int sum = 0;
+			// for (IntWritable val : values) {
+			// 	sum += val.get();
+			// }
+			// result.set(sum);
+			context.write(key, null);
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+		Configuration conf = new Configuration();
+		Job job = Job.getInstance(conf, "word count");
+		job.setJarByClass(WordCount.class);
+		job.setMapperClass(WCMapper.class);
+		job.setReducerClass(WCReducer.class);
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(IntWritable.class);
+		FileInputFormat.addInputPath(job, new Path(args[0]));
+		FileOutputFormat.setOutputPath(job, new Path(args[1]));
+		System.exit(job.waitForCompletion(true) ? 0 : 1);
+	}
+}
