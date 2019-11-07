b4614a50a6f5e65c3886c3754182de1a30213b1b
Nathaniel Olson <natsolson@gmail.com>
2019-11-07 15:42:15 +0000

    import FW

--- Temperature.java
+++ Temperature.java
@@ -3,6 +3,7 @@ import java.util.StringTokenizer;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.FloatWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
